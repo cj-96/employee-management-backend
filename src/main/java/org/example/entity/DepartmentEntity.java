a775c92649e8a63c7879ac404b54fafbe510a987
cj-96 <chamfer1996@gmail.com>
2025-01-17 22:49:49 +0530

    DepartmentEntity created

---
+++ src/main/java/org/example/entity/DepartmentEntity.java
@@ -0,0 +1,26 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "departments")
+public class DepartmentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<EmployeeEntity> employees;
+}
