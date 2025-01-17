5a727796c4ef91c4de1d3c62b30969fa418c0f0b
cj-96 <chamfer1996@gmail.com>
2025-01-17 22:49:30 +0530

    AttendanceEntity created

---
+++ src/main/java/org/example/entity/AttendanceEntity.java
@@ -0,0 +1,31 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "attendance")
+public class AttendanceEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private EmployeeEntity employee;
+
+    private LocalDate date;
+
+    private String status;
+
+    private String checkInTime;
+
+    private String checkOutTime;
+}
