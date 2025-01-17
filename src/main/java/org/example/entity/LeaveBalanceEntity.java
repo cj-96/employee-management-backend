53c378a2e3f1814ca2956e5d97044b70b854536a
cj-96 <chamfer1996@gmail.com>
2025-01-17 22:50:12 +0530

    LeaveBalanceEntity created

---
+++ src/main/java/org/example/entity/LeaveBalanceEntity.java
@@ -0,0 +1,26 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "leave_balances")
+public class LeaveBalanceEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private EmployeeEntity employee;
+
+    private String leaveType;
+
+    private int balance;
+}
