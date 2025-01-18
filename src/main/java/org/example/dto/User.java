895f08357646b1a7a7411589a98417e350bd0132
cj-96 <chamfer1996@gmail.com>
2025-01-18 11:22:42 +0530

    User created

--- src/main/java/org/example/dto/User.java
+++ src/main/java/org/example/dto/User.java
@@ -1,13 +1,25 @@
 package org.example.dto;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.example.entity.Role;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 public class User {
+
+    private String username;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private Employee employee;
 }
