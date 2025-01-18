232dcb9e2704d291249f60982339174f6a1a5829
cj-96 <chamfer1996@gmail.com>
2025-01-18 08:57:17 +0530

    User dto created

---
+++ src/main/java/org/example/dto/User.java
@@ -0,0 +1,13 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+}
