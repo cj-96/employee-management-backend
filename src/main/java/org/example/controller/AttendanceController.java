b717bb408872dd1897f0fa513a94a09f97287b0b
cj-96 <chamfer1996@gmail.com>
2025-01-20 16:03:51 +0530

    AttendanceController created

--- src/main/java/org/example/controller/AttendanceController.java
+++ src/main/java/org/example/controller/AttendanceController.java
@@ -2,51 +2,67 @@ package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.dto.Attendance;
+import org.example.dto.SuccessResponse;
 import org.example.service.AttendanceService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
-@RequestMapping("/api/attendance")
+@RequestMapping("/attendance")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class AttendanceController {
 
     private final AttendanceService service;
+    private final String SUCCESS_STATUS = "success";
 
-    // Create Attendance
     @PostMapping
-    public ResponseEntity<Attendance> persist(@RequestBody Attendance attendance) {
-        Attendance createdAttendance = service.persist(attendance);
-        return ResponseEntity.ok(createdAttendance);
+    public ResponseEntity<SuccessResponse> persist(@RequestBody Attendance attendance) {
+        SuccessResponse successResponse =SuccessResponse.builder()
+                .status(SUCCESS_STATUS)
+                .data(service.persist(attendance))
+                .build();
+
+        return ResponseEntity.ok().body(successResponse);
     }
 
-    // Get All Attendance Records
     @GetMapping
-    public ResponseEntity<List<Attendance>> retrieveAll() {
-        List<Attendance> attendanceList = service.retrieveAll();
-        return ResponseEntity.ok(attendanceList);
+    public ResponseEntity<SuccessResponse> retrieveAll() {
+        SuccessResponse successResponse =SuccessResponse.builder()
+                .status(SUCCESS_STATUS)
+                .data(service.retrieveAll())
+                .build();
+
+        return ResponseEntity.ok().body(successResponse);
     }
 
-    // Get Attendance by ID
     @GetMapping("/{id}")
-    public ResponseEntity<Attendance> retrieveById(@PathVariable Long id) {
-        Attendance attendance = service.retrieveById(id);
-        return ResponseEntity.ok(attendance);
+    public ResponseEntity<SuccessResponse> retrieveById(@PathVariable Long id) {
+        SuccessResponse successResponse =SuccessResponse.builder()
+                .status(SUCCESS_STATUS)
+                .data(service.retrieveById(id))
+                .build();
+
+        return ResponseEntity.ok().body(successResponse);
     }
 
-    // Update Attendance
     @PutMapping("/{id}")
-    public ResponseEntity<Attendance> update(@PathVariable Long id, @RequestBody Attendance attendanceDetails) {
-        Attendance updatedAttendance = service.update(id, attendanceDetails);
-        return ResponseEntity.ok(updatedAttendance);
+    public ResponseEntity<SuccessResponse> update(@PathVariable Long id, @RequestBody Attendance attendanceDetails) {
+        SuccessResponse successResponse =SuccessResponse.builder()
+                .status(SUCCESS_STATUS)
+                .data(service.update(id,attendanceDetails))
+                .build();
+
+        return ResponseEntity.ok().body(successResponse);
     }
 
-    // Delete Attendance
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable Long id) {
+    public ResponseEntity<SuccessResponse> delete(@PathVariable Long id) {
         service.delete(id);
-        return ResponseEntity.ok("Attendance record deleted successfully.");
+        SuccessResponse successResponse = SuccessResponse.builder()
+                .status(SUCCESS_STATUS)
+                .build();
+        return ResponseEntity.ok().body(successResponse);
     }
 }
